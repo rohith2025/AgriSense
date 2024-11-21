bc8c40405685d4e2c525400db0ef62ea227eef76
rohith2025 <145789486+rohith2025@users.noreply.github.com>
2024-11-21 20:18:59 +0530

    Update InfiniteScrollLogos.jsx

--- src/components/InfiniteScrollLogos.jsx
+++ src/components/InfiniteScrollLogos.jsx
@@ -5,7 +5,7 @@ const InfiniteScrollLogos = () => {
   return (
     <div className="my-6 mx-10">
       <Marquee direction="left" loop={0}>
-        <p className='text-green-800'>This is a beta version currently under development. You may encounter occasional issues. Thank you for your support during this exciting phase!</p>
+        <p className='text-green-800'>This is a beta version currently under development. You may encounter occasional issues. All images used on this site are sourced from the internet and are credited to their respective owners. Thank you for your support during this exciting phase!</p>
       </Marquee>
     </div>
   );
